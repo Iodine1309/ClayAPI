03960058d17aeea133cd303465364744191f157d
Iodine1309 <Ian@Ian-PC>
2014-01-30 03:53:08 -0500

    changed abstract RecipeBase, to a class

--- Clay/src/com/clay/recipe/RecipeBase.java
+++ Clay/src/com/clay/recipe/RecipeBase.java
@@ -2,11 +2,53 @@ package com.clay.recipe;
 
 import org.bukkit.inventory.Recipe;
 
-public abstract class RecipeBase {
-
-	public abstract String getName();
-	public abstract Recipe getRecipe();
-	public abstract RecipeTypeEnum getType();
+public class RecipeBase {
+	private RecipeTypeEnum recipe_type;
+	private Recipe recipe_recipe;
+	private String recipe_name;
+	
+	public RecipeBase(String filename) {
+		loadRecipe(filename);
+	}
+	
+	public RecipeBase(String name, RecipeTypeEnum type, Recipe recipe) {
+		recipe_type = type;
+		recipe_name = name;
+		recipe_recipe = recipe;
+	}
+	
+	public boolean loadRecipe(String filename) {
+		return false;
+	}
+	
+	public boolean saveRecipe(String filename) {
+		return false;
+	}
+	
+	public String getName() {
+		return recipe_name;
+	}
+	
+	public void setName(String name) {
+		recipe_name = name;
+	}
+	
+	public void setType(RecipeTypeEnum type) {
+		recipe_type = type;
+	}
+	
+	public RecipeTypeEnum getType() {
+		return recipe_type;
+	}
+	
+	public Recipe getRecipe() {
+		return recipe_recipe;
+	}
+	
+	public void setRecipe(Recipe recipe) {
+		recipe_recipe = recipe;
+	}
+	
 	
 	public Recipe loadRecipeFromFile() {
 		
